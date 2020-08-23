51aa8d32bcffba006689acb5d47aaae1ac64d721
Vysakh_v <vysakhv1998@github.com>
2020-08-23 22:13:21 +0530

    final commit

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-ECHO is on.
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("slide-share");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+        myIndex = 1
+    }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 2000);
+}
